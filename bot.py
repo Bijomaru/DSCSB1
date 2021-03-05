1156590d5d0778e7dcea8f02e4e128db758a46d2
Bijomaru <31664348+Bijomaru@users.noreply.github.com>
2021-03-05 22:21:28 +0100

    updates to refer personal nick

--- bot.py
+++ bot.py
@@ -8,15 +8,16 @@ class MyClient(discord.Client):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
-        print('------')
+        print(self.guilds)
+
 
     async def on_message(self, message):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
-
+        
         if message.content.startswith('!hello'):
-            await message.reply('Hello!', mention_author=True)
+            await message.reply('Hello {user}'.format(user=message.author.nick), mention_author=True)
 
 client = MyClient()
 client.run(utilities.token)

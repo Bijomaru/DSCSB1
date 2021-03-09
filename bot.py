9d71e7d60d376dbaa4d50c7abbaf97d0277ea368
Bijomaru <31664348+Bijomaru@users.noreply.github.com>
2021-03-09 16:47:33 +0100

    Added invitetracker, session Role and permissions

--- bot.py
+++ bot.py
@@ -11,7 +11,10 @@ This is the guide with the commands.
     !end_session - ends your session
 '''
 
-bot = commands.Bot(command_prefix='!')
+# bot = commands.Bot(command_prefix='!')
+
+def find_invite_by_code(self, invite_list, code):
+    return [inv for inv in invite_list if inv.code == code][0]
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -20,14 +23,20 @@ class MyClient(discord.Client):
         print(self.user.id)
         print(self.guilds)
         guild = self.guilds[0]
-        # if guild.name == 'Social Cooking':
-        #     await self.guilds[0].system_channel.send('Bot connected')
+        if guild.name == 'Social Cooking':
+            await self.guilds[0].system_channel.send('Bot connected')
+        current_invites = await guild.invites()
 
     async def on_member_join(self, member):
         guild = member.guild
-        if guild.system_channel is not None:
-            to_send = 'Welcome {member.mention} to {guild.name}!'.format(member, guild)
-            await guild.system_channel.send(to_send)
+        updated_invites = await member.guild.invites()
+        for invite in current_invites:
+            if invite.uses < find_invite_by_code(updated_invites, invite.code).uses:
+                print(f"Member {member.name} Joined")
+                print(f"Invite Code: {invite.code}")
+                print(f"Inviter: {invite.inviter}")
+                current_invites = updated_invites
+                return
 
 
     async def on_message(self, message):
@@ -56,12 +65,26 @@ class MyClient(discord.Client):
                 await message.reply('Session {session} already exists'.format(session=session_name), mention_author=True)
                 return
 
-            await guild.create_category_channel(session_name)
+            role = await guild.create_role(name=session_name, permissions = discord.Permissions._from_value(103812608))
 
-            cat = discord.utils.get(guild.categories, name=session_name)
+            cat = await guild.create_category_channel(session_name)
+            
+            overwrite = discord.PermissionOverwrite()
+            overwrite.view_channel = False
+            await cat.set_permissions(guild.default_role, overwrite=overwrite)
 
-            await cat.create_text_channel('Text')
-            await cat.create_voice_channel('Voice')
+            overwrite = discord.PermissionOverwrite()
+            overwrite.view_channel = True
+            await cat.set_permissions(message.author, overwrite=overwrite)
+            await cat.set_permissions(role, overwrite=overwrite)
+
+            txt = await cat.create_text_channel('Text')
+            vc = await cat.create_voice_channel('Voice')
+            invite_txt = await txt.create_invite(max_uses=10, unique=True, temporary=True, reason='')
+            invite_vc = await vc.create_invite(max_uses=10, unique=True, temporary=True, reason='')
+            
+            await txt.send(invite_txt)
+            await txt.send(invite_vc)
             await message.reply('Session {session} has been created.'.format(session=session_name), mention_author=True)
             await guild.system_channel.send('Session {session} has been created'.format(session=session_name))
 
@@ -80,16 +103,12 @@ class MyClient(discord.Client):
             for channel in cat.channels:
                 await channel.delete()
             await self.get_channel(cat.id).delete()
+
+            await discord.utils.get(guild.roles, name=session_name).delete()
+
             await message.reply('Session {session} ended'.format(session=session_name), mention_author=True)
             await guild.system_channel.send('Session {session} ended'.format(session=session_name))
             return
-
-
-            # await guild.create_category_channel(session_name)
-            # await message.reply('Session {session} has been created.'.format(session=session_name), mention_author=True)
-            
-            return
-
 
         if message.content.startswith('!hello'):
             await message.reply('Hello {user}'.format(user=user), mention_author=True)
@@ -99,6 +118,11 @@ class MyClient(discord.Client):
         if message.content.startswith('!guide'):
             await message.reply(guide_txt)
             return
+
+
+
+intents = discord.Intents.default()
+intents.members = True
 
 client = MyClient()
 client.run(utilities.token)
